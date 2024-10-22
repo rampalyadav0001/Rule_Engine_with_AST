f97da1a371eaf89dd52545cd5c6c5dd9918c53aa
RAMPAL <rampalyadav23082001@gmail.com>
2024-10-22 10:37:00 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/rule', 
+   // baseURL: 'http://localhost:8000/api/rule', 
+    baseURL:``${import.meta.env.VITE_BASE_URL}/api/rule;
 });
 
 export default instance;
